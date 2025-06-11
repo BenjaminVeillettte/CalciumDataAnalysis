0dbd8eac13df2df837d6bbb550924605ced9aee9
Benjamin veillette <bevei16@ulaval.ca>
2025-06-11 17:13:28 -0400

    DocStringwriting and modif Preprocess

--- PredictionAlgorithme.py
+++ PredictionAlgorithme.py
@@ -1,13 +1,19 @@
+"""
+This script is used to predict spiking activity from calcium imaging data using two different algorithms: Cascade and OASIS.
+It reads the data from the given csv and then applies the prediction algorithms.
+"""
+
+
+
 import os 
 import numpy as np
 import sys
 from sys import path
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 import ruamel.yaml as yaml
 yaml = yaml.YAML(typ='rt')
 
-
+#Path to read (Optionnal since its only to predict so you can put them directly into another script)
 folder_path = os.path.join(
     os.path.expanduser("~"), 
     "OneDrive - Cégep de Shawinigan", 
@@ -20,6 +26,13 @@ file_path = os.path.join(folder_path, file_data)
 
 
 def ReadData (file_path):
+   """
+   Read the data from the CSV file and return the relevant columns.
+    Args:
+    file_path (str): The path to the CSV file containing the data.
+    Returns:
+    tuple: A tuple containing the time, ephys, gcamp, and spikes data as numpy arrays.
+   """
    df = pd.read_csv(
       file_path,
       sep=",",          
@@ -29,13 +42,15 @@ def ReadData (file_path):
    Order = ['Time', '1 Spikes', 'gcamp', 'spikes']
    df = df[Order]
 
-   time = df["Time"]
-   ephys = df["1 Spikes"]
-   gcamp = df["gcamp"]
-   spikes = df["spikes"] 
+   time = df["Time"].to_numpy()
+   ephys = df["1 Spikes"].to_numpy()
+   gcamp = df["gcamp"].to_numpy()
+   spikes = df["spikes"].to_numpy()
 
    return time, ephys, gcamp, spikes
 
+
+# Where the Oasis folder located 
 folder_pathOasis = os.path.join(
     os.path.expanduser("~"), 
     "OneDrive - Cégep de Shawinigan", 
@@ -46,12 +61,14 @@ folder_pathOasis = os.path.join(
     "OASIS-master"
 )
 
-# Ajouter le chemin pour les modules oasis
+# Adding the Oasis folder to the Python path
 path.append(folder_pathOasis)
+
+# Importing the deconvolve function from the oasis package
 from oasis.functions import deconvolve
 
 
-# 2) Chemin vers Cascade-master (contenant le sous-dossier cascade2p/)
+# Where the Cascade folder located
 folder_pathCascade = os.path.join(
     os.path.expanduser("~"), 
     "OneDrive - Cégep de Shawinigan", 
@@ -62,6 +79,8 @@ folder_pathCascade = os.path.join(
     "Cascade-master"
 )
 
+
+# Adding the Cascade folder to the Python path
 if not os.path.isdir(folder_pathCascade):
  raise FileNotFoundError(f"{folder_pathCascade!r} n'existe pas")
 
@@ -73,12 +92,12 @@ if folder_pathCascade not in sys.path:
 os.chdir(folder_pathCascade)
 print('Current working directory: {}'.format( os.getcwd() ))
 
-
+# Importing Cascade functions
 from cascade2p import checks, cascade
 
 checks.check_packages()
 
-
+# Base path to save results
 base = os.path.join(
   os.path.expanduser("~"),
   "OneDrive - Cégep de Shawinigan",
@@ -91,7 +110,15 @@ base = os.path.join(
 frame_rate = 100 # in Hz
 
 def predictionCascade(base, gcamp):
-   
+   """
+   Predict spiking activity from calcium imaging data using the Cascade algorithm.
+   Args:
+   base (str): The base path where the results will be saved.
+   gcamp (numpy.ndarray): The calcium imaging data as a 1D numpy array.
+   Returns:
+   numpy.ndarray: The predicted spiking activity as a 2D numpy array.
+   """
+   # Prepare the data for the Cascade model
    datatoCascade = gcamp
 
    pathCascade = os.path.join(base, "ArrayCascade.npy")
@@ -116,6 +143,8 @@ def predictionCascade(base, gcamp):
          raise ValueError(f"dff doit être 2D, got ndim={traces.ndim}")
        return traces
    traces = load_neurons_x_time(pathCascade)
+
+   #Checking the lenght of the recording
    print('Number of neurons in dataset:', traces.shape[0])
    print('Number of timepoints in dataset:', traces.shape[1])
 
@@ -131,18 +160,27 @@ def predictionCascade(base, gcamp):
 
 
 def predictionOasis(base, time, gcamp):
-   
-   csv_name = "DonneesOASIS.csv"
-   csv_path = os.path.join(base, csv_name)
+    """
+    Predict spiking activity from calcium imaging data using the OASIS algorithm.
+    Args:
+    base (str): The base path where the results will be saved.
+    time (numpy.ndarray): The time vector as a 1D numpy array.
+    gcamp (numpy.ndarray): The calcium imaging data as a 1D numpy array.
+    Returns:
+    numpy.ndarray: The predicted spiking activity as a 1D numpy array.
+    """
+   # Prepare the data for the OASIS model
+    csv_name = "DonneesOASIS.csv"
+    csv_path = os.path.join(base, csv_name)
 
-   df = pd.DataFrame({
-      "Time": time,
-      "3 PMT1": gcamp,
-     })
+    df = pd.DataFrame({
+       "Time": time,
+       "3 PMT1": gcamp,
+       })
    
-   df.to_csv(csv_path, index=False)
+    df.to_csv(csv_path, index=False)
    
-   data = pd.read_csv(csv_path)  
-   y = data['3 PMT1'].values
-   c, s, b, g, lam = deconvolve(y, penalty=1)
-   return s 
+    data = pd.read_csv(csv_path)  
+    y = data['3 PMT1'].values
+    c, s, b, g, lam = deconvolve(y, penalty=1)
+    return s 
--- PreprocessData.py
+++ PreprocessData.py
@@ -1,3 +1,10 @@
+"""    
+This script first processes calcium imaging data by applying a low-pass filter, correcting for bleaching, and calculating the ΔF/F and Z-score.
+It then computes the signal-to-noise ratio (SNR) and normalizes noise levels. The processed data is saved to a new CSV file, and plots of the results are generated
+to make sure the processing was successful.
+"""
+
+
 import os
 import glob
 import pandas as pd
@@ -28,14 +35,46 @@ window = 30 * frame_rate
 
 
 def troncage(array, RemovedTime):
+    """
+    Remove points in the array to 
+    avoid the first and last desired 
+    seconds of data to remove recording artifacts.
+    
+    Parameters:
+    array (np.ndarray): The input array to be truncated.
+    RemovedTime (int): The number of points to remove from the start and end.
+    Returns:
+    np.ndarray: The truncated array.
+    """ 
     return array[RemovedTime : len(array) - RemovedTime]
 
-def LowPass(signal):
-    b, a = butter(5, Wn, btype="low", analog=False, output="ba")
-    return filtfilt(b, a, signal)
+def LowPass(signal, time_array):
+    """
+    Applying a low-pass filter to the signal to remove noise above 10 Hz.
+    Parameters:
+    signal (np.ndarray): The input signal to be filtered.
+    Returns:
+    np.ndarray: The filtered signal.
+    """
+    mask = np.isfinite(signal) & np.isfinite(time_array)
+    calcium_clean = signal[mask]
+    time_clean = time_array[mask]
 
-def Double_exponential(t, a, b, c, d, e):
-    return a * np.exp(-b * t) + c * np.exp(-d * t) + e
+    b, a = butter(5, 10, btype="low", analog=False, output="ba")
+    return filtfilt(b, a, calcium_clean), time_clean
+
+def Double_exponential(t, const, amp_fast, amp_slow, tau_slow, tau_multiplier):
+    '''Compute a double exponential function with constant offset.
+    Parameters:
+    t       : Time vector in seconds.
+    const   : Amplitude of the constant offset. 
+    amp_fast: Amplitude of the fast component.  
+    amp_slow: Amplitude of the slow component.  
+    tau_slow: Time constant of slow component in seconds.
+    tau_multiplier: Time constant of fast component relative to slow. 
+    '''
+    tau_fast = tau_slow*tau_multiplier
+    return const+amp_slow*np.exp(-t/tau_slow)+amp_fast*np.exp(-t/tau_fast)
 
 def Rising_exponential(t, const, amp, tau):
     '''Compute a rising exponential function with constant offset.
@@ -48,15 +87,29 @@ def Rising_exponential(t, const, amp, tau):
     return const + amp * (1 - np.exp(-t / tau))
 
 def bleaching_correct(calcium, time_array):
+    """
+    Correction for bleaching in fiber photometry using a double exponential.
+    If the recording have a baseline that is going up, a rising exponential is used.
+    Parameters:         
+    calcium (np.ndarray): The calcium signal to be corrected.
+    time_array (np.ndarray): The time vector corresponding to the calcium signal.
+    Returns:
+    np.ndarray: The corrected calcium signal after applying the bleaching correction.
+    np.ndarray: The fitted curve used for correction.
+    """
+
+    #Removing Nan values and giving them the same lenghth
     mask = np.isfinite(calcium) & np.isfinite(time_array)
     calcium_clean = calcium[mask]
     time_clean = time_array[mask]
 
+    #Setting initial and bounds for fitting
     max_sig = np.max(calcium_clean)
     inital_params = [max_sig/2, max_sig/4, max_sig/4, 3600, 0.1] 
     bounds = ([0      , 0      , 0      , 600  , 0],
           [max_sig, max_sig, max_sig, 36000, 1])
     
+    #Setting the end plateau for rising exponential defined as the 5th percentile of the last 1000 points(10 last seconds)
     plateau  = np.percentile(calcium_clean[-1000:], 5)
     popt, _ = curve_fit(Double_exponential, time_clean, calcium_clean, p0=inital_params, bounds=bounds, maxfev=1000)
     fitted = Double_exponential(time_clean, *popt)
@@ -75,11 +128,29 @@ def bleaching_correct(calcium, time_array):
         return calcium_clean - fitted2, fitted2
 
 def bleaching_correct2(calcium):
-    b,a = butter(2, 0.001, btype='high', fs=frame_rate)
+    """
+    Another method to correct for bleaching in fiber photometry using a high-pass filter.
+    Parameters:
+    calcium (np.ndarray): The calcium signal to be corrected.
+    Returns:
+    np.ndarray: The corrected calcium signal after applying the high-pass filter.
+    """    
+    #Note the baseline is harder to find with this method
+    b,a = butter(2, 0.005, btype='high', fs=frame_rate)
     CalciumHigh = filtfilt(b, a, calcium, padtype='even')
     return CalciumHigh
 
 def ScalingDFF(signal, baseline):
+    """
+    Normalizing the calcium trace to make it into ΔF/F.
+    The methode used here is to divided the signal by the baseline found with the curve fitting and multiply by 100 to get a percentage.
+    I also correct the baseline to be always positive by making the minimum value 0.
+    Parameters:
+    signal (np.ndarray): The calcium signal to be normalized.
+    baseline (np.ndarray): The baseline signal used for normalization.
+    Returns:
+    np.ndarray: The normalized calcium signal in ΔF/F (%).
+    """
     dff = (signal/baseline)*100
 
     mini = np.nanmin(dff)
@@ -88,6 +159,18 @@ def ScalingDFF(signal, baseline):
     return dff
 
 def sliding(data, win, step=200):
+    """
+    Creation of a sliding window to segment the data into smaller parts.
+    That way we can get an appromixation note based on the whole recording but only using a small part of it.
+    It is more precise than using the whole recording.
+    Parameters:
+    data (np.ndarray): The input data to be segmented.
+    win (int): The size of the sliding window.
+    step (int): The step size for the sliding window.
+    Returns:
+    list: A list of segments created from the input data.
+    """ 
+    #Note if the window = steps it will not overlap anymore.
     segments = []
     L = len(data)
     for i in range(0, L - win + 1, step):
@@ -98,17 +181,43 @@ def sliding(data, win, step=200):
 
 
 def noise_std_from_diff(signal):
+    """
+    Calculate the noise level from the standard deviation of the differences in the signal.
+    This method is based on the Median Absolute Deviation (MAD) of the differences.
+    Parameters:
+    signal (np.ndarray): The input signal from which to calculate the noise level.
+    Returns:
+    float: The calculated noise level across the recording.
+    """
     diffs = np.diff(signal)
     mad_diff = np.median(np.abs(diffs - np.median(diffs)))
     return mad_diff / (0.6745 * np.sqrt(2))
 
 def calcPercentile(segments, perc):
+    """
+    Calculating the percentile of the segments created from the sliding function.
+    It will be used to deteremine the SNR by defining a threshold for the signal at 95%.
+    Parameters:
+    segments (list): A list of segments created from the sliding function.
+    perc (int): The percentile to calculate.
+    Returns:
+    float: The calculated percentile value across the segments."""
 
     values = [np.percentile(seg, perc) for seg in segments]
     return np.nanmean(values)
 
 #Ajout calcul SNR en dépassant 2 sigma selon cote Z et bruit = std deviation (MAD) fois 1,42...
 def PeakFinder(Zscore):
+    """
+    Finding the peak to calculate the SNR from the Z-score.
+    This function identifies peaks in the Z-score data that are above a threshold (2 standard deviations).
+    When it is bewlow, it will take maximum of the peak defined and average of all the peaks found.
+    Parameters:
+    Zscore (np.ndarray): The Z-score data from which to find peaks.
+    Returns:
+    float: The average peak value found in the Z-score data.
+    """
+
     Peak = []
     PeakMean = []
     for value in Zscore:
@@ -125,13 +234,13 @@ def PeakFinder(Zscore):
     Moyenne = np.nanmean(PeakMean)
     return Moyenne
 
-
+#Main loop to read all the csv files in the input folder
 pattern = os.path.join(input_folder, "*.csv")
 for file_path in glob.glob(pattern):
  
     df = pd.read_csv(file_path, sep=",", decimal=".", header=0)
     
- 
+    #Taking the required columns to process 
     required_cols = ["Time", "1 Spikes", "gcamp", "spikes"]
     for col in required_cols:
         if col not in df.columns:
@@ -139,27 +248,30 @@ for file_path in glob.glob(pattern):
         else:
             df = df[required_cols]
 
+    #Making sure they are array
     time_arr   = df["Time"].to_numpy()
     ephys_arr  = df["1 Spikes"].to_numpy()
     gcamp_arr  = df["gcamp"].to_numpy()
     spikes_arr = df["spikes"].to_numpy()
 
+    #Removing artefacts
     time_tr   = troncage(time_arr, RemovedTime)
     ephys_tr  = troncage(ephys_arr, RemovedTime)
     gcamp_tr  = troncage(gcamp_arr, RemovedTime)
     spikes_tr = troncage(spikes_arr, RemovedTime)
 
 
-    gcamp_filt = LowPass(gcamp_tr)
+    gcamp_filt, time_correct = LowPass(gcamp_tr, time_tr)
 
+    gcamp_corr, curve = bleaching_correct(gcamp_filt, time_correct)
 
-    gcamp_corr = bleaching_correct(gcamp_filt, time_tr)
-
-    Calcium_Dff, Baseline = ScalingDFF(gcamp_corr)
+    Calcium_Dff = ScalingDFF(gcamp_corr, curve)
     Calcium_Zscore = zscore(gcamp_corr, nan_policy="omit")
 
-    Gcamp_corriger = bleaching_correct(gcamp_tr, time_tr)
-    Fscaled, Baseline2 = ScalingDFF(Gcamp_corriger)
+    #Calucation of noise and SNR with Df/f and Z-score. No filtering is applied to the raw gcamp trace to get the real noise level.
+    #SNR is defined as ration of signal peak to noise.
+    Gcamp_corriger, baseline = bleaching_correct(gcamp_tr, time_tr)
+    Fscaled = ScalingDFF(Gcamp_corriger, baseline)
     Calcium_NoProccess_Z = zscore(Gcamp_corriger, nan_policy="omit")
     PeakZ = PeakFinder(Calcium_NoProccess_Z)
     NoiseZ = noise_std_from_diff(Calcium_NoProccess_Z)
@@ -170,13 +282,15 @@ for file_path in glob.glob(pattern):
     SNR95 = calcPercentile(segments, 95) / Noise
     Value95 = calcPercentile(segments, 95)
 
+    #Calculating the normalized noise level
+    #This is the median of the absolute difference between each point divided by the square root of the frame rate and is given in percentage.
     Normalized_noise_levels = (np.nanmedian(np.abs(np.diff(Fscaled, axis=-1)), axis=-1) / np.sqrt(frame_rate)) * 100
 
     print(f"Normalized noise : {Normalized_noise_levels}")
     print(f"SNR with DFF : {SNR95}")
     print(f"SNR with Z score : {SNRZ}")
 
-    df_out = pd.DataFrame({
+    a = ({
         "Time"             : time_tr,
         "1 Spikes"         : ephys_tr,
         "spikes"           : spikes_tr,
@@ -188,39 +302,45 @@ for file_path in glob.glob(pattern):
         "Normalized Noise Level" : Normalized_noise_levels * np.ones_like(time_tr),
         "SNR95%"           : SNR95 * np.ones_like(time_tr),
         "SNR with Z score" : SNRZ * np.ones_like(time_tr),
-        "Baseline"         : Baseline * np.ones_like(time_tr),
+        "Baseline"         : baseline,
     })
-    # Si vous souhaitez inclure les spikes tronqués :
-    # df_out["spikes_tr"] = spikes_tr
 
+    df_out = pd.DataFrame.from_dict(a, orient="index")
+    df_out = df_out.transpose()
+
+    #Saving the processed data to a new CSV file and making sure the output folder worked. I also made this since array can have different lengths.
    
     fname = os.path.basename(file_path)
-    out_filename = f"processed_{fname}"           
+    out_filename = f"{fname}"           
     out_path    = os.path.join(output_folder, out_filename)
     df_out.to_csv(out_path, index=False)
 
     print(f"→ '{fname}' traité et enregistré sous '{out_path}'.")
 
 
+    #Plotting the results to visualize the processing steps and ensure correctness 
     fig, axes = plt.subplots(2, 2, figsize=(10, 6))
     ax1, ax2, ax3, ax4 = axes.flatten()
     
     ax1.plot(time_tr, gcamp_tr, color="red")
-    ax1.set_title("Trace Calcium brute")
+    ax1.plot(time_correct, curve, color="blue")
+    ax1.set_title("Raw calcium trace")
     ax1.set_ylabel("Fluorescence brute")
+    ax1.set_xlim()
     
-    ax2.plot(time_tr, gcamp_corr, color="blue")
-    ax2.set_title("Après filtrage 10 Hz & correction bleaching")
+    
+    ax2.plot(time_correct, gcamp_corr, color="blue")
+    ax2.set_title("After filter 10 Hz & correction bleaching")
     ax2.set_ylabel("Fluorescence corrigée")
-    
-    ax3.plot(time_tr, Calcium_Dff, color="green")
+    ax2.set_xlim()
+
+    ax3.plot(time_correct, Calcium_Dff, color="green")
     ax3.axhline(Value95, color="lightgrey", linestyle="--", label="Seuil SNR95 %")
-    ax3.axhline(Baseline, color="dimgrey", linestyle="--", label="Baseline")
     ax3.set_title("ΔF/F (%)")
     ax3.set_ylabel("ΔF/F (%)")
     ax3.legend(loc="best")
     
-    ax4.plot(time_tr, Calcium_Zscore, color="purple")
+    ax4.plot(time_correct, Calcium_Zscore, color="purple")
     ax4.set_title("Z-score")
     ax4.set_ylabel("Z-score (σ)")
 
